68d53e8842a3d171b20b93c2a598d0665c4d074a
Sebastian Onofrei <137564517+SebastianOnofrei@users.noreply.github.com>
2024-03-04 13:34:20 +0200

    Revert "feat: Adeed the Footer component"

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,6 @@
 import AboutUs from "./components/organisms/AboutUs/AboutUs";
 import BestSelling from "./components/organisms/BestSelling/BestSelling";
 import Categories from "./components/organisms/Categories/Categories";
-import Footer from "./components/organisms/Footer/Footer";
 import Header from "./components/organisms/Header/Header";
 import Hero from "./components/organisms/Hero/Hero";
 import Testimonials from "./components/organisms/Testimonials/Testimonials";
