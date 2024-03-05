bca5626b58bda83f8d97b21f33aeb745305d2128
talidag <talida.ganciu@gmail.com>
2024-03-05 11:43:47 +0200

    fix merge conflicts

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,4 @@
 import Header from "./components/organisms/Header/Header";
-<<<<<<< HEAD
-import Hero from "./components/organisms/Hero/Hero";
-import Testimonials from "./components/organisms/Testimonials/Testimonials";
-=======
 import Contacts from "./components/pages/Contacts";
 import Filters from "./components/pages/Filters";
 import Home from "./components/pages/Home";
@@ -10,7 +6,6 @@ import Missing from "./components/pages/Missing";
 import Products from "./components/pages/Products";
 import Profile from "./components/pages/Profile";
 import ShoppingCart from "./components/pages/ShoppingCart";
->>>>>>> 95d452c... feat: Added react-router and links and small improvments to the Header
 import "./index.scss";
 import { Route, Routes } from "react-router-dom";
 
@@ -18,13 +13,6 @@ function App() {
   return (
     <div className="app">
       <Header />
-<<<<<<< HEAD
-      <Hero />
-      <BestSelling />
-      <AboutUs />
-      <Categories />
-      <Testimonials />
-=======
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
@@ -36,7 +24,6 @@ function App() {
         <Route path="*" element={<Missing />}></Route>
       </Routes>
       {/* <Footer /> */}
->>>>>>> 95d452c... feat: Added react-router and links and small improvments to the Header
     </div>
   );
 }
