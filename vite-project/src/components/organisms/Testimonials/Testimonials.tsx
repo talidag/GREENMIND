ae3464fc5b788fe77c38b11d4fe75a2e438e3015
talidag <talida.ganciu@gmail.com>
2024-03-06 13:56:11 +0200

    feat: Added the swipper slides for the Testimonials component

--- vite-project/src/components/organisms/Testimonials/Testimonials.tsx
+++ vite-project/src/components/organisms/Testimonials/Testimonials.tsx
@@ -4,10 +4,29 @@ import testimonial1 from "../../../assets/testimonial1.png";
 import testimonial2 from "../../../assets/testimonial2.png";
 import slider from "../../../assets/slider.svg";
 import TestimonialsCard from "../../molecules/TestimonialsCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Testimonials = () => {
   const testimonialsTitle = "What customers say about GREEMIND?";
   const testimonialsData = [
+    {
+      text: "Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+      name: "John Doe",
+      ocupation: "Youtuber",
+      picture: testimonial1,
+      rating: 4.5,
+    },
+    {
+      text: "Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+      name: "John Doe",
+      ocupation: "Youtuber",
+      picture: testimonial2,
+      rating: 4,
+    },
     {
       text: "Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
       name: "John Doe",
@@ -29,10 +48,29 @@ const Testimonials = () => {
         <Title title={testimonialsTitle} />
         <img src={slider} alt="" />
       </div>
+
       <div className="testimonials__cards">
-        {testimonialsData.map((testimonial, index) => (
-          <TestimonialsCard key={index} {...testimonial} />
-        ))}
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={1.6}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {testimonialsData.map((testimonial, index) => (
+            <SwiperSlide key={index}>
+              <TestimonialsCard key={index} {...testimonial} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
